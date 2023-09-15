741984dcf6b6e59b7211521fd8b52ae917ad35ed
Duc Nguyen <minhducnguyen189@gmail.com>
2023-09-15 19:23:45 +0700

    update example for serving static UI

---
+++ frontend-integration/src/main/java/com/springboot/project/frontend/integration/FrontendIntegrationApplication.java
@@ -0,0 +1,11 @@
+package com.springboot.project.frontend.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendIntegrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendIntegrationApplication.class, args);
+    }
+}
---
+++ frontend-integration/src/main/java/com/springboot/project/frontend/integration/config/StaticResourceConfig.java
@@ -0,0 +1,16 @@
+package com.springboot.project.frontend.integration.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+
+}
