1d0e2f68387835c2e8ab207d858a3c56d02e6ce7
Duc Nguyen <minhducnguyen189@gmail.com>
2023-10-19 22:13:44 +0700

    add example for spring boot actuator and graceful shutdown

---
+++ actuator-and-graceful-shutdown/src/main/java/com/springboot/project/controller/SampleController.java
@@ -0,0 +1,21 @@
+package com.springboot.project.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/v1/messages", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> getMessages() throws InterruptedException {
+        Thread.sleep(25000);
+        return ResponseEntity.ok(Arrays.asList("Message 1", "Message 2"));
+    }
+
+}
