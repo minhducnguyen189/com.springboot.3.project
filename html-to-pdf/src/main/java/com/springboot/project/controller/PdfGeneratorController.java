8e9dfa940c9908f44ef07c6071a33e263bd3be8d
Duc Nguyen <minhducnguyen189@gmail.com>
2024-03-17 21:43:00 +0700

    html to pdf example

---
+++ html-to-pdf/src/main/java/com/springboot/project/controller/PdfGeneratorController.java
@@ -0,0 +1,38 @@
+package com.springboot.project.controller;
+
+import com.springboot.project.service.PdfGeneratorService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+@Controller
+public class PdfGeneratorController {
+
+    private final PdfGeneratorService pdfGeneratorService;
+
+    @Autowired
+    public PdfGeneratorController(PdfGeneratorService pdfGeneratorService) {
+        this.pdfGeneratorService = pdfGeneratorService;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/generatePdfFile")
+    public void generatePdfFile(HttpServletResponse response, @RequestParam("contentToGenerate") String contentToGenerate) throws IOException {
+        ByteArrayInputStream byteArrayInputStream = this.pdfGeneratorService.convertHtmlToPdf(contentToGenerate);
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment; filename=file.pdf");
+        IOUtils.copy(byteArrayInputStream, response.getOutputStream());
+    }
+
+}
---
+++ html-to-pdf/src/main/java/com/springboot/project/service/PdfGeneratorService.java
@@ -0,0 +1,27 @@
+package com.springboot.project.service;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Service;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+@Service
+public class PdfGeneratorService {
+
+    public ByteArrayInputStream convertHtmlToPdf(String htmlContent) {
+        Document document = Jsoup.parse(htmlContent, "UTF-8");
+        document.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
+        String documentHtml = document.html();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.setDocumentFromString(documentHtml);
+        renderer.layout();
+        renderer.createPDF(outputStream, false);
+        renderer.finishPDF();
+        return new ByteArrayInputStream(outputStream.toByteArray());
+    }
+
+}
