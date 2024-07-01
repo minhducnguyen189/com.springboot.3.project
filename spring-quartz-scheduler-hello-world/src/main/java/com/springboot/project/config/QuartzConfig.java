bb6f530804dd1669c26be18ee893073dc27c18f6
ducnguyen <minhducnguyen189@gmail.com>
2024-07-01 22:30:25 +0700

    add simple quartz config with spring boot

---
+++ spring-quartz-scheduler-hello-world/src/main/java/com/springboot/project/config/QuartzConfig.java
@@ -0,0 +1,33 @@
+package com.springboot.project.config;
+
+import com.springboot.project.service.SimpleJob;
+import org.quartz.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Configuration
+public class QuartzConfig {
+
+    @Bean
+    public CommandLineRunner run(Scheduler scheduler) {
+        return (String[] args) -> {
+            JobDetail job = JobBuilder.newJob(SimpleJob.class)
+                    .usingJobData("param", "value") // add a parameter
+                    .build();
+
+            Date afterFiveSeconds = Date.from(LocalDateTime.now().plusSeconds(30)
+                    .atZone(ZoneId.systemDefault()).toInstant());
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .startAt(afterFiveSeconds)
+                    .build();
+
+            scheduler.scheduleJob(job, trigger);
+        };
+    }
+
+}
---
+++ spring-quartz-scheduler-hello-world/src/main/java/com/springboot/project/service/SimpleJob.java
@@ -0,0 +1,19 @@
+package com.springboot.project.service;
+
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.text.MessageFormat;
+
+public class SimpleJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        JobDataMap dataMap = jobExecutionContext.getJobDetail().getJobDataMap();
+        String param = dataMap.getString("param");
+        System.out.println(MessageFormat.format("Job: {0}; Param: {1}",
+                getClass(), param));
+    }
+}
