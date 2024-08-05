cb2663d7d4f491622403e1bb54588e4fd04cca09
ducnguyen <minhducnguyen189@gmail.com>
2024-08-05 22:14:26 +0700

    update cay bang 2

--- cay-bang-grocery-api-2/src/main/java/com/springboot/project/CayBangGroceryApp2.java
+++ cay-bang-grocery-api-2/src/main/java/com/springboot/project/CayBangGroceryApp2.java
@@ -1,7 +1,11 @@
 package com.springboot.project;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CayBangGroceryApp2 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(CayBangGroceryApp2.class, args);
     }
 }
